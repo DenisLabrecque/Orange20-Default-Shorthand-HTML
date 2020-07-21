c5c6f7101f3396c7e81c8ae28841c3a3be4b0908
Denis G. Labrecque <denis.g.labrecque@gmail.com>
2020-07-20 21:48:44 -0500

    Progress loading a special title element

--- mark-down/mark-down.js
+++ mark-down/mark-down.js
@@ -1,11 +1,14 @@
-fetch('mark-down/mark-down.html')
-
 customElements.define('mark-down',
    class extends HTMLElement {
       connectedCallback() {
          setTimeout(() => {
             let markdown = this.innerHTML
             let contents = this
+            //fetch('common/showdown-2.0.0.js').then(() => {
+            //   let converter = new showdown.Converter()
+            //   let html = converter.makeHtml(markdown)
+            //   contents.innerHTML = html
+            //})
             require(['common/showdown-2.0.0'], (showdown) => {
                let converter = new showdown.Converter()
                let html = converter.makeHtml(markdown)
